e976266abdde7450ed684b20bc6ac3ec0d6b1769
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-03-10 18:18:23 +0000

    Update tree_disease.pyx

--- tree_disease.pyx
+++ tree_disease.pyx
@@ -3,20 +3,19 @@ import matplotlib.pyplot as plt
 import random 
 import matplotlib.animation as animation
 import time
-
 N = 200
 beta = 0.3
 T = 1
-end = False
+
 
 # Initialise the grid and temporary grid.
-grid = np.random.choice([0,1], p = [0.2, 0.8]).reshape(N, N)
-tmp = np.zeroes(shape = (N,N))
+grid = np.random.choice([0,1], N*N, p = [0.2, 0.8]).reshape(N, N)
+tmp = np.zeros(shape = (N,N))
 
 # For loop to randomly choose infected cells within sub area of grid.
-for i in range(N/2):
-    for j in range(N/2):
-        if random.uniform(0, 1) < 0.8:
+for i in range(50, 150):
+    for j in range(50, 150):
+        if random.uniform(0, 1) > 0.8:
             grid[i][j] = 2
 
 # Function to count the number of infected neighbours.
@@ -32,14 +31,15 @@ def Von_Neuman_neigh(grid, x, y):
 
 # Function to process one iteration.
 def Tree(frame_num, img, grid):
-    tmp = np.zeroes(shape = (N,N))
+    stop = False
+    tmp = np.zeros(shape = (N,N))
     start = time.time()
     for i in range(N):
         for j in range(N):
             neigh = Von_Neuman_neigh(grid, i, j)
             # Check if the edge of the grid for infected.
             if (i == N-1 or j == N-1) and (grid[i][j] >= 2 and grid[i][j] < 2 + T):
-                end = True
+                stop = True
                 break
             # "Roll the dice" neigh times to see if tree becomes infected.
             elif neigh != 0 and grid[i][j] == 1:
@@ -51,7 +51,7 @@ def Tree(frame_num, img, grid):
                         neigh -= 1
             elif grid[i][j] >= 2 and grid[i][j] < 2 + T:
                 tmp[i][j] = grid[i][j] + 0.1
-        if end:
+        if stop:
             break
     end = time.time()
     print(end - start)
@@ -64,7 +64,7 @@ def main():
     ax.set_yticklabels([])
     ax.set_xticklabels([])
     img = ax.imshow(grid, interpolation = 'nearest')
-    ani = animation.FuncAnimation(fig, Tree, fargs = (img, grid, N, ), frames = 10, interval = 50, save_count = 50)
+    ani = animation.FuncAnimation(fig, Tree, fargs = (img, grid, ), frames = 10, interval = 50, save_count = 50)
     plt.show()
 
 if __name__ == '__main__':
