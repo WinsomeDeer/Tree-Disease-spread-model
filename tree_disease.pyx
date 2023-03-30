95d9a39b1475cf11bd36c4634627be0d0b2a58bd
Patrik <103080404+WinsomeDeer@users.noreply.github.com>
2023-03-30 20:57:28 +0100

    Update tree_disease.pyx

--- tree_disease.pyx
+++ tree_disease.pyx
@@ -3,58 +3,63 @@ import matplotlib.pyplot as plt
 import random 
 import matplotlib.animation as animation
 import time
-N = 100
-beta = 0.1
-T = 1
 
-def Infect(M):
+N = 50
+beta = 0.5
+T = 12
+gamma = 0.6
+infected_grid = np.zeros(shape = (N,N))
+
+# Initialise random grid.
+def random_grid(gamma):
+    grid = np.random.choice([0, 1, 2], N*N, p = [0.95 - gamma, gamma, 0.05]).reshape(N, N)
+    # Initalise infected grid
     for i in range(N):
         for j in range(N):
-            if np.random.uniform(0,1) < 0.99:
-                M[i][j] = 2
-
-# Function to count the number of infected neighbours.
-def Von_Neuman_neigh(M, x, y):
-    total = 0
-    Dv = [[-1,0], [1,0], [0,1], [0,-1]]
-    for dv in Dv:
-        dx = x + dv[0]
-        dy = y + dv[1]
-        if (dx >= 0 and dy >= 0 and dx < N and dy < N) and (M[dx][dy] >= 2 and M[dx][dy] < 2 + T):
-            total += 1
-    return total
+            if grid[i][j] == 2:
+                infected_grid[i][j] = 1
+    return grid
 
 # Function to process one iteration.
-def Tree(frame_num, img, grid):
+def Tree(frame_num, img, grid, infected_grid):
     tmp = np.zeros(shape = (N,N))
-    start = time.time()
+    Dv = [[-1,0], [1,0], [0,1], [0,-1]]
+    # For loop to update grid.
     for i in range(N):
         for j in range(N):
-            neigh = Von_Neuman_neigh(grid, i, j)
-            # "Roll the dice" neigh times to see if tree becomes infected.
-            if neigh != 0 and grid[i][j] == 1:
-                while neigh > 0 and tmp[i][j] < 2:
-                    if random.uniform(0, 1) < beta:
-                        tmp[i][j] = 2
-                    neigh -= 1
-            elif grid[i][j] >= 2 and grid[i][j] < 2 + T:
-                tmp[i][j] = grid[i][j] + 0.1
-    end = time.time()
-    print(end - start)
+            # Check if the cell is infected.
+            if grid[i][j] == 2:
+                for dv in Dv:
+                    dx, dy = i + dv[0], j + dv[1]
+                    if (dx >= 0 and dy >= 0 and dx < N and dy < N) and (grid[dx][dy] == 1) and (np.random.uniform(0,1) < beta):
+                        tmp[dx][dy] = 2
+                        infected_grid[dx][dy] = 1
+                tmp[i][j] = grid[i][j]
+            else:
+                tmp[i][j] = grid[i][j]
+    # For loops to update infected grid.
+    for i in range(N):
+        for j in range(N):
+            if infected_grid[i][j] == T:
+                tmp[i][j] = 0
+            elif infected_grid[i][j] >= 1 and infected_grid[i][j] < T:
+                infected_grid[i][j] += 1
+    print(infected_grid)
+    time.sleep(1)
     img.set_data(tmp)
-    grid = tmp
+    grid[:] = tmp[:]
     return img
 
 def main():
-    # Initialise the grid.
-    grid = np.random.choice([0,1], N*N, p = [0, 1]).reshape(N, N)
+    # Initialise the grid and infected grid.
+    grid = random_grid(gamma)
+    infected = np.zeros(shape = (N, N))
     grid.astype('float64')
-    Infect(grid)
     fig, ax = plt.subplots()
     ax.set_yticklabels([])
     ax.set_xticklabels([])
     img = ax.imshow(grid, interpolation = 'nearest')
-    ani = animation.FuncAnimation(fig, Tree, fargs = (img, grid ), frames = 10, interval = 50, save_count = 50)
+    ani = animation.FuncAnimation(fig, Tree, fargs = (img, grid, infected ), frames = 10, interval = 50, save_count = 50)
     plt.show()
 
 if __name__ == '__main__':
